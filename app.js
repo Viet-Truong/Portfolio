9569dd5348ad5f5bec17dc6bff9aaacde8e08f18
Viet Truong <viettruong0825@gmail.com>
2023-01-05 23:53:22 +0700

    make ui recent work item

--- app.js
+++ app.js
@@ -1,13 +1,60 @@
-import Typed from './node_modules/typed.js/lib/typed';
+import Typed from "./node_modules/typed.js/lib/typed";
 
-var Typed = require('./typed.min.js');
-var typed = new Typed('#header-search-input', {
-	strings: ["", "I'm a Front-End Developer", "I'm a Designer", "I'm a Android Developer"],
-    attr: 'placeholder',
+var Typed = require("./typed.min.js");
+var typed = new Typed("#header-search-input", {
+    strings: [
+        "",
+        "I'm a Front-End Developer",
+        "I'm a Designer",
+        "I'm a Android Developer",
+    ],
+    attr: "placeholder",
     startDelay: 50,
     typeSpeed: 50,
     backSpeed: 50,
     smartBackspace: true,
     loop: true,
-    loopCount: Infinity
+    loopCount: Infinity,
 });
+
+// Recent work logic
+var container = document.querySelector(".recentWork__container");
+arr = [
+    {
+        src: "./img/tiktok.JPG",
+        type: "web",
+    },
+    {
+        src: "./img/chatapp.JPG",
+        type: "web",
+    },
+    {
+        src: "./img/music.png",
+        type: "web",
+    },
+    {
+        src: "./img/port.png",
+        type: "lading_page",
+    },
+    {
+        src: "./img/blog.png",
+        type: "lading_page",
+    },
+    {
+        src: "./img/appmusic.jpg",
+        type: "app",
+    },
+    {
+        src: "./img/quanlinhahang.png",
+        type: "app",
+    },
+    {
+        src: "./img/tour.png",
+        type: "design",
+    },
+];
+
+var container_img = document.createElement("div");
+container_img.className = "recentWork__container--img";
+
+container.append(container_img);
